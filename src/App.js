5dd82f9d891ead214d114b1a7f7a9ad15513d70a
Ashish Desai <desai.ashish76@gmail.com>
2023-03-12 17:08:58 -0400

    added nasa api logic to get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import "./App.css";
+import { BASE_URL, API_KEY } from "./constants";
+import axios from 'axios';
 
 function App() {
+  
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(() => {
+  console.log('response started');
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        // console.log(res);
+        setNasaData(res.data);
+      })
+      .catch(err => console.log('error getting data from NASA API', err))
+  }, []);
+
   return (
     <div className="App">
       <p>
